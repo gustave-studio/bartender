b5115a2e152a070f799e23e45680097f4cf55297
gustave-studio <gustave0222@gmail.com>
2022-02-05 07:25:24 +0900

    ✨メッセージの表示を1文字ずつに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('今日は、どういったお酒を飲まれますか？')
+  const [message, setMessage] = useState('どういったお酒にしましょうか？')
+  const [displayMessage, setDisplayMessage] = useState('')
+  const [displayChoices, setDisplayChoices] = useState(false)
   const [whisky, setWhisky] = useState(false)
   const [choices, setChoices] = useState(['ウィスキー', 'ビール'])
 
@@ -11,6 +13,7 @@ function App() {
       setWhisky(true)
       setMessage('燻製のようなスモーキーな香りがあっても大丈夫でしょうか？')
       setChoices(['大丈夫', '苦手かな'])
+      setDisplayChoices(false)
     }
 
     if ('ウィスキー' === choices[key]) {
@@ -18,18 +21,43 @@ function App() {
       setWhisky(true)
       setMessage('どんなウィスキーにしますか？')
       setChoices(['好きな味を言うから、オススメ教えて', 'ウィスキーのジャンルで選びたいな'])
+      setDisplayChoices(false)
     }
   }
 
+  const [i, setI] = useState(1)
+
+  function sleep(milliseconds: number) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+
+  const setInteger = async () => {
+    let length = message.length
+    for (let i = 0; i < (length + 1); i++) {
+      setDisplayMessage(message.slice(0, i))
+      console.log(i)
+      await sleep(50);
+    }
+    await sleep(100);
+    setDisplayChoices(true)
+  }
+
+  useEffect(() => {
+    setInteger()
+  }, [message]);
+
   return (
     <div className="App">
-      <h2>バーテンダー:「{message}」</h2>
-      {choices.map((choice, key) => (
-        <div>
-          {console.log(key)}
-          <button key={key} onClick={() => selectMenu(choice, key)}>{choice}</button>
-        </div>
-      ))}
+      バーテンダー: {displayMessage}
+    <section id="bartender">
+    </section>
+    {choices.map((choice, key) => (
+          <span key={key} style={{ display: displayChoices ? '' : 'none' }}>
+            <button key={key} onClick={() => selectMenu(choice, key)}>{choice}</button>
+            <br />
+          </span>
+        ))}
     </div>
   );
 }
