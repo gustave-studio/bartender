b045a4a71c00e3326e209a01f1c67ec1b3baeea6
gustave-studio <gustave0222@gmail.com>
2022-02-16 04:31:10 +0900

    🔥ヘッダーからサウンドを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,10 @@
-import React, { useState }  from 'react';
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline'
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline'
-import ReactHowler from 'react-howler'
+import React from 'react';
 
-type HeaderPropsType = {
-  playing: boolean;
-  setPlaying: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-const Header = function (props: HeaderPropsType) {
-  const [playing, setPlaying] = useState(false)
-
+const Header = function () {
   return (
     <div className="header">
       <h1>VR Bar</h1>
-        BGM : Gunshot Straight
-        <br />
-        Artist : MusMus
-        <br />
-        Sound
-        <br />
-        { props.playing
-          ?<PauseCircleOutlineIcon
-            onClick={() => props.setPlaying((state) => !state)}/>
-          :<PlayCircleOutlineIcon
-            onClick={() => props.setPlaying((state) => !state)}/>
-        }
-        <ReactHowler
-          src={`${process.env.REACT_APP_SOUND_FILE}`}
-          playing={props.playing}
-        />
-        </div>
+    </div>
   );
 };
 
